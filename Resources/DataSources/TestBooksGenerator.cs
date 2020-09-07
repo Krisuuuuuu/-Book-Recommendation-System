6d06d1fad3f8d79475f34ce76bbb4db381de3f64
Krisuuuuuu <krisu1997@onet.pl>
2020-09-07 18:45:14 +0200

    Added generator of test values

---
+++ Resources/DataSources/TestBooksGenerator.cs
@@ -0,0 +1,94 @@
+﻿using Model.Shop;
+using System.Collections.Generic;
+
+namespace Resources.DataSources
+{
+    public static class TestBooksGenerator
+    {
+        public static ICollection<Book> GenerateBooks()
+        {
+            ICollection<Book> books = new List<Book>();
+
+            Book book1 = new Book()
+            {
+                Authors = "Andrzej Sapkowski",
+                Title = "The Witcher: Blood of Elves",
+                Genres = "Dark Fantasy|Action|Magic|Slav|Fiction|Non-Humans|Life-History"
+            };
+
+            Book book2 = new Book()
+            {
+                Authors = "John Ronald Reuel Tolkien",
+                Title = "The Lord of the Rings: The Return of the King",
+                Genres = "Fantasy|Action|Magic|Fiction|Adventure|Non-Humans"
+            };
+
+            Book book3 = new Book()
+            {
+                Authors = "George Raymond Richard Martin",
+                Title = "The Song of the Ice and Fire",
+                Genres = "Fantasy|Action|Magic|Fiction|Adventure|Non-Humans|Policy"
+            };
+
+            Book book4 = new Book()
+            {
+                Authors = "Jenson Button",
+                Title = "Jenson Button: Life to the Limit: My Autobiography",
+                Genres = "Autobiography|Life-History|Adventure|Sports|Policy|Inspiration|F1|Retrospection"
+            };
+
+            Book book5 = new Book()
+            {
+                Authors = "Adam Bielecki",
+                Title = "From under Frozen Eyelids",
+                Genres = "Autobiography|Life-History|Adventure|Sports|Mountains|Inspiration|Mountaineering|Retrospection"
+            };
+
+            Book book6 = new Book()
+            {
+                Authors = "Ashlee Vans",
+                Title = "Elon Musk",
+                Genres = "Autobiography|Life-History|Science|Power of the Mind|Inspiration|Genius|Retrospection"
+            };
+
+
+            Book book7 = new Book()
+            {
+                Authors = "Adam Mickiewicz",
+                Title = "Pan Tadeusz",
+                Genres = "History|Action|Policy|Epos|Art|Romanticism|Bard"
+            };
+
+
+            Book book8 = new Book()
+            {
+                Authors = "Illiad",
+                Title = "Homer",
+                Genres = "History|Action|Mythology|Epos|Art|Antique|Greece"
+            };
+
+            Book book9 = new Book()
+            {
+                Authors = "Make It Before God",
+                Title = "Hanna Krall",
+                Genres = "History|Action|War|Judes|Nazism|Retrospection|Extermination|Holocaust"
+            };
+
+            Book book10 = new Book()
+            {
+                Authors = "Band of Brothers",
+                Title = "Stephen Ambrose",
+                Genres = "History|Action|War|Nazism|Retrospection|Extermination|Soldiers"
+            };
+
+            Book book11 = new Book()
+            {
+                Authors = "Teutonic Knights",
+                Title = "Henryk Sienkiewicz",
+                Genres = "History|Action|War|Middle Ages|Love|Knights"
+            };
+
+            return books;
+        }
+    }
+}
---
+++ Resources/DataSources/TestUsersGenerator.cs
@@ -0,0 +1,43 @@
+﻿using Model.Shop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Resources.DataSources
+{
+    public static class TestUsersGenerator
+    {
+        public static ICollection<Client> GenerateClients()
+        {
+            ICollection<Client> clients = new List<Client>();
+            ICollection<Order> orders = new List<Order>();
+
+            Book book1 = new Book()
+            {
+                Authors = "Andrzej Sapkowski",
+                Title = "The Witcher: Blood of Elves",
+                Genres = "Dark Fantasy|Action|Magic|Slav|Fiction|Non-Humans|Life-History"
+            };
+
+            Book book2 = new Book()
+            {
+                Authors = "John Ronald Reuel Tolkien",
+                Title = "The Lord of the Rings: The Return of the King",
+                Genres = "Fantasy|Action|Magic|Fiction|Adventure|Non-Humans"
+            };
+
+            Order order1 = new Order(1, DateTime.Now);
+            order1.Products.Add(book1);
+            orders.Add(order1);
+
+            Order order2 = new Order(2, DateTime.Now);
+            order2.Products.Add(book2);
+            orders.Add(order2);
+
+            Client client = new Client("Christopher", "Crown");
+            client.Orders = orders;
+
+            return clients;
+        }
+    }
+}
